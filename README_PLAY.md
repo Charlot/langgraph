31d069b769c2eb339fa46d31b011d6062bdf685c
song.wang <7wangsong@gmail.com>
2025-06-26 01:11:16 +0800

    add graph

--- README_PLAY.md
+++ README_PLAY.md
@@ -15,6 +15,7 @@ source .venv/bin/activate
 uv pip install -r requirements-dev.txt
 uv pip install --group libs/langgraph/pyproject.toml:dev
 
+
 #uv sync --active --inexact 
 
 ```
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -6,24 +6,46 @@ from IPython.display import display, Image
 import operator
 
 class State(TypedDict):
-        hello_a: str
-        hello_b: str
-        hello_c: str
-        hello_d: str
+    hello_a: str
+    hello_b: str
+    hello_c: str
+    hello_d: str
+    hello_e: str
+    hello_f: str
 
 def node_a(state: State) -> State:
+    print("----> node a is called")
+
     return {"hello_a": "world_a", "hello_a_1": "world_a_1"}
 
 
 def node_b(state: State) -> State:
+    print("----> node b is called")
+
     return {"hello_b": "world_c"}
 
 def node_c(state: State) -> State:
+    print("----> node c is called")
+
     return {"hello_c": "world_c",
             "hello_a": "world_c_a"}
 
-def node_d(state: State) -> State:
+def node_d(state: State) -> State:    
+    print("----> node d is called")
+
     return {"hello_d": "world_d"}
+
+
+def node_e(state: State) -> State:  
+    print("----> node e is called")
+
+    return {"hello_e": "world_e"}
+
+
+def node_f(state: State) -> State:
+    print("----> node f is called")
+
+    return {"hello_f": "world_f"}
 
 def node_d_route_fun(state: State)->bool:
     return True
@@ -35,16 +57,66 @@ def test_graph_build() -> None:
     builder.add_node("b", node_b)
     builder.add_node("c", node_c)
     builder.add_node("d", node_d)
+    builder.add_node("e", node_e)
+    builder.add_node("f", node_f)
+
     builder.set_entry_point("a")
     builder.add_edge("a", "b")
     builder.add_edge("a", "c")
-    builder.add_conditional_edges("a", node_d_route_fun, {True: "d", False: "__end__"} )
+    builder.add_conditional_edges("a", node_d_route_fun, {True: "d", False: "f"} )
+    builder.add_edge("a", "e")
+    builder.add_edge("e", "b")
+    builder.add_edge("b", "f")
+    builder.add_edge("c", "f")
+    builder.add_edge("d", "f")
+
+
     graph = builder.compile()
     graph.nodes["a"].node
 
     # display(Image(graph.get_graph().draw_mermaid_png()))
-    # print(graph.get_graph().draw_ascii())
-
+    print(graph.get_graph().draw_ascii())
+#                 +-----------+                  
+#                 | __start__ |                  
+#                 +-----------+                  
+#                       *                        
+#                       *                        
+#                       *                        
+#                     +---+                      
+#                   **| a |.                     
+#                *** *+---+*....                 
+#           *****  **   .    ** ....             
+#        ***     **     .      *    ...          
+#     ***      **       .       **     ....      
+# +---+       *         .         *        ..    
+# | e |      *          .         *         .    
+# +---+     *           .         *         .    
+#      *    *           .         *         .    
+#       *  *            .         *         .    
+#        **             .         *         .    
+#      +---+            .       +---+     +---+  
+#      | b |**          .       | c |   **| d |  
+#      +---+  **        .      *+---+***  +---+  
+#               ***     .    ** *****            
+#                  **   .  *****                 
+#                    ** . ***                    
+#                     +---+                      
+#                     | f |                      
+#                     +---+                      
+#                       *                        
+#                       *                        
+#                       *                        
+#                  +---------+                   
+#                  | __end__ |                   
+#                  +---------+                   
+# ----> node a is called
+# ----> node b is called
+# ----> node c is called
+# ----> node d is called
+# ----> node e is called
+# ----> node b is called
+# ----> node f is called
+# ----> node f is called
 
     result = graph.invoke({"helloa": "there"})
     print("result:----->", result)
