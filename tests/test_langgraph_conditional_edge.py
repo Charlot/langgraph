3332ec588c4801ef65b14a4748c24f5bb6a8b40b
song.wang <song.wang@cyclone-robotics.com>
2025-07-07 18:03:17 +0800

    add test

--- tests/test_langgraph_conditional_edge.py
+++ tests/test_langgraph_conditional_edge.py
@@ -9,33 +9,49 @@ class State(TypedDict):
     hello_d: str
 
 def node_a(state: State) -> State:
+    print("node_a")
     return {"hello_a": "hello_a"}
 
-def route_fun(state: State)->bool:
-    return 0
+def route_fun_a_1(state: State)->bool:
+    print("route_fun_a_1")
+    return 1
 
 def node_b(state: State) -> State:
+    print("node_b")
     return {"hello_b": "hello_b"}
 
 def node_c(state: State) -> State:
+    print("node_c")
     return {"hello_c": "hello_c"}
 
 def node_d(state: State) -> State:
+    print("node_d")
     return {"hello_d": "hello_d"}
 
+def node_e(state: State) -> State:
+    print("node_e")
+    return {"hello_e": "hello_e"}
 
-def test_conditional_edge() -> None:
+
+def test_conditional_edge_branch_merge() -> None:
+    """简单merge测试    
+    """
     builder=StateGraph(State)
     builder.add_node("a",node_a)
     builder.add_node("b",node_b)
     builder.add_node("c",node_c)
     builder.add_node("d",node_d)
+    builder.add_node("e",node_e)
 
     builder.add_edge(START, "a")
-    builder.add_node("branch", lambda x:x)
-    builder.add_conditional_edges("a", path=route_fun,path_map={0:"branch",1:"d"})
+    builder.add_node("branch", lambda x:{})
+    builder.add_conditional_edges("a", path=route_fun_a_1,path_map={0:"branch",1:"d"})
     builder.add_edge("branch", "b")
     builder.add_edge("branch", "c")
+    builder.add_edge("b", "e")
+    builder.add_edge("c", "e")
+    builder.add_edge("d", "e")
+    builder.add_edge("e", END)
     
     graph = builder.compile()
     
@@ -44,8 +60,11 @@ def test_conditional_edge() -> None:
     graph.invoke(input={})
 
 
+
+
+
 if __name__ == "__main__":
-    test_conditional_edge()
+    test_conditional_edge_branch_merge()
 
 
 
